7cc0ea7bd70c02303d47947f28a5787458934a67
SafarGo <159808426+SafarGo@users.noreply.github.com>
2026-04-28 11:11:38 +0300

    New inventory commit

--- TechnoArrowGame/Assets/Scripts/LobbyScene/DontDestroyController.cs
+++ TechnoArrowGame/Assets/Scripts/LobbyScene/DontDestroyController.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroyController : MonoBehaviour
 {
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
+        SceneManager.sceneLoaded += OnLoaded;
+    }
+
+    void OnLoaded(Scene scene, LoadSceneMode mode)
+    {
+        GameObject spawnpoint = GameObject.Find("SpawnPoint");
+        transform.position = spawnpoint.transform.position;
     }
 }
